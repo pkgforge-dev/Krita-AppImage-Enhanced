22dd37ee5b59db341da3caa2e671d63172f1f5d6
Link4Electronics <link4electronics@gmail.com>
2026-09-10 23:15:25 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q krita | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q krita | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -24,11 +24,9 @@ quick-sharun /usr/bin/krita* \
   /usr/lib/qt6/plugins/sqldrivers \
   /usr/lib/libproxy/libpxbackend-1.0.so
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
